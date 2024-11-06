2f733df919c722bb0f4a3ebce74be7223439f02f
Logan Pearce <lapearce@l-jn2t3m97wy.local>
2024-11-06 15:22:49 -0500

    fixed sql

--- pages/Derivation.py
+++ pages/Derivation.py
@@ -6,5 +6,8 @@ import streamlit as st
 def read_markdown_file(markdown_file):
     return Path(markdown_file).read_text()
 
+with open('Bright-Stars-Select.ipynb') as f:
+    st.download_button('Download this notebook',f)
+
 intro_markdown = read_markdown_file("Bright-Stars-Select.md")
 st.markdown(intro_markdown, unsafe_allow_html=True)
