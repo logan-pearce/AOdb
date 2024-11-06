581c573880a48c9afff9f6ed68db9c1eb8cd250b
Logan Pearce <lapearce@l-jn2t3m97wy.local>
2024-11-06 15:33:02 -0500

    downloading derivation

--- pages/Derivation.py
+++ pages/Derivation.py
@@ -6,8 +6,8 @@ import streamlit as st
 def read_markdown_file(markdown_file):
     return Path(markdown_file).read_text()
 
-with open('Bright-Stars-Select.ipynb', 'rb') as f:
-    st.download_button('Download this notebook',f)
+with open('bright-stars-select.tar.gz', 'rb') as f:
+    st.download_button('Download this notebook and files',f)
 
 intro_markdown = read_markdown_file("Bright-Stars-Select.md")
 st.markdown(intro_markdown, unsafe_allow_html=True)
